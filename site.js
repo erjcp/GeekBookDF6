b7c3374ba6dda72c4d6075a582218b3918dc0c8d
dishap125 <dpate050@fiu.edu>
2019-03-28 18:37:50 -0400

    initial

--- site.js
+++ site.js
@@ -50,8 +50,10 @@ app.get('/profile', (req, res) => {
 app.get('/cart', (req, res) => {
   var cartList = [];
   var saveLaterList = [];
+  var cartTotal = 0;
+  var saveTotal = 0;
 
-  let sql = `select b.title, b.price, c.customerId, ci.cartType from Book b, Cart c, CartItem ci where (ci.orderId = c.customerId and ci.bookId = b.bookCode)`;
+  let sql = `select b.title, b.price, c.customerId, ci.cartType, ci.quantity from Book b, Cart c, CartItem ci where (ci.orderId = c.customerId and ci.bookId = b.bookCode and c.customerID = 0000)`;
   let query = db.query(sql, (err, rows, results) => {
     if (err) {
       console.log(sql);
@@ -61,16 +63,18 @@ app.get('/cart', (req, res) => {
       title : rows[i].title,
       price : rows[i].price,
       customerId : rows[i].customerId,
-      cartType : rows[i].cartType
+      cartType : rows[i].cartType,
+      quantity : rows[i].quantity
       }
       if(item.cartType == 1){
         cartList.push(item);
+        cartTotal+= (item.price * item.quantity);
       }
       else{saveLaterList.push(item);}
-        
+      saveTotal+= (item.price * item.quantity);  
       console.log(item);
     }
-    res.render('cart', {"cartList": cartList, "saveLaterList" : saveLaterList});
+    res.render('cart', {"cartList": cartList, "saveLaterList" : saveLaterList, "cartTotal" : cartTotal, "saveTotal": saveTotal});
   });
   for(var j = 0; j < cartList; j++){
     console.log(cartList[i]);
@@ -125,125 +129,3 @@ app.post('/', function (req, res){
     res.send(results);
   });
 });
-
-
-
-/*var http = require("http");
-var fs = require("fs");
-const port = 6117;
-var mysql = require("mysql");
-var request = require("request");
-var Query = "";
-var pQuery = "";
-var Table = "";
-var completeResult = "";
-var response;
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "website",
-  password: "abc123",
-  database: "geekbook"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to mySQL server.");
-});
-
-request.post(
-  "localhost",
-  {
-    json: { key: "value" }
-  },
-  function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(response);
-    }
-  }
-);
-
-function getS(str) {
-  Table = str.substring(str.indexOf("=") + 1, str.indexOf("&"));
-  pQuery = str.substring(str.lastIndexOf("=") + 1);
-  Query = pQuery + " " + Table;
-  console.log("getS table: " + Table);
-  //console.log("getS Query: " + pQuery);
-  //console.log("getS Result Query: " + Query);
-}
-
-String.prototype.replaceAll = function(str1, str2, ignore) {
-  return this.replace(
-    new RegExp(
-      str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g, "\\$&"),
-      ignore ? "gi" : "g"
-    ),
-    typeof str2 == "string" ? str2.replace(/\$/g, "$$$$") : str2
-  );
-};
-
-http.createServer(function (request,response){
-  console.log("url: " + request.url);
-  console.log("method: " + request.method);
-
-  let req_url = request.url.toString();
-
-  if (request.method === "POST") {
-    let body = "";
-    request.on("data", chunk => {
-      body += chunk.toString();
-      getS(body);
-      //console.log(body);
-      pQuery = pQuery.replaceAll("~", "=");
-      console.log("parsed: " + pQuery);
-
-      con.query(pQuery, function(err, result, fields) {
-        if (err) throw err;
-        completeResult = result;
-        console.log(result);
-        globalResponse = response;
-      });
-      //});
-    });
-    request.on("end", () => {
-      console.log("test");
-      setTimeout(function() {
-        var JSONx = JSON.stringify(completeResult);
-        response.write(JSONx);
-        console.log("test2");
-        response.end();
-        console.log("test3");
-      }, 1000);
-    });
-
-  } else if (request.url === '/') {
-    fs.readFile('./index.html',function(err,data){
-      response.writeHead(200,{'Content-Type': 'text/html'});
-      response.write(data);
-      response.end();
-    });
-  }
-  else if(req_url.endsWith(".css")) {
-    let res_url = ".";
-    res_url += request.url.toString();
-    console.log("res_url = " + res_url);
-
-    fs.readFile(res_url, function(err, data){
-      if (err) throw err;
-      response.writeHead(200,{'Content-Type': 'text/css'});
-      response.write(data);
-      response.end();
-    })  
-  } 
-  else if(req_url.endsWith(".html")){
-    let res_url = ".";
-    res_url += request.url.toString();
-    console.log("res_url = " + res_url);
-
-    fs.readFile(res_url, function(err, data){
-      if (err) throw err;
-      response.writeHead(200,{'Content-Type': 'text/html'});
-      response.write(data);
-      response.end();
-    })  
-  } 
-}).listen(port); */
